46aa04715c71f3c4ca053f22f27bfd09fbfde108
robmoore <rob.moore@gmail.com>
2014-12-16 14:46:21 -0500

    - Using same testing approach as for HLL. Hence, removing 10K word text. - Refactored code to hide usage of priority queue from client.

--- KMV.hs
+++ KMV.hs
@@ -4,45 +4,50 @@
 
 module KMV where
 
-import           Control.Applicative
+import qualified Data.Digest.XXHash   as XXH (xxHash)
+import qualified Data.PQueue.Max      as DPM (MaxQueue, deleteMax, empty,
+                                              findMax, insert, null, singleton,
+                                              size)
+import           Data.Word            (Word32)
+import           System.Random
 
-import qualified Data.ByteString.Lazy.Char8 as BC (pack)
-import qualified Data.Digest.XXHash         as XXH (xxHash)
-import qualified Data.List                  as DL (nub)
-import qualified Data.PQueue.Max            as DPM (MaxQueue, deleteMax, empty,
-                                                    findMax, insert, null,
-                                                    singleton, size)
-import           Data.Word                  (Word32)
+import qualified Data.Binary          as DB (encode)
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.List.Split      as DLS (chunksOf)
+import qualified Data.Word            as DW (Word32)
 
-mkHash :: Fractional a => String -> a
-mkHash s = fromIntegral h / fromIntegral (maxBound :: Word32) -- resize to {0,1}
-   where h = XXH.xxHash $ BC.pack s
+mkValue :: Fractional a => BS.ByteString -> a
+mkValue bs = fromIntegral h / fromIntegral (maxBound :: Word32) -- resize to {0,1}
+   where h = XXH.xxHash bs
 
 condInsert :: Ord a => a -> Int -> DPM.MaxQueue a -> DPM.MaxQueue a
-condInsert x k mq
-   | DPM.null mq = DPM.singleton x
-   | DPM.size mq < k = DPM.insert x mq -- haven't reached K so insert
-   | x < DPM.findMax mq = DPM.deleteMax $ DPM.insert x mq
+condInsert v k mq
+   | DPM.null mq = DPM.singleton v
+   | DPM.size mq < k = DPM.insert v mq -- haven't reached K so insert
+   | v < DPM.findMax mq = DPM.deleteMax $ DPM.insert v mq
    | otherwise = mq -- value isn't larger than max so no-op
 
-estCount :: RealFrac a => Int -> DPM.MaxQueue a -> Int
-estCount k mq = if sz < k then sz else round d
-    where sz = DPM.size mq
-          d = fromIntegral (k - 1) / DPM.findMax mq
+calcE :: (Fractional a, Integral b) => DPM.MaxQueue a -> b -> a
+calcE mq k = fromIntegral (k - 1) / DPM.findMax mq
+
+card :: [BS.ByteString] -> Int -> Int
+card bs k = if sz < k then sz else round $ calcE mq k
+    where vs = map mkValue bs
+          mq = foldr (`condInsert` k) DPM.empty vs
+          sz = DPM.size mq
 
 main :: IO ()
 main = do
-    let k = 10
-    let filename = "google-10000-english.txt"
+    let k = 100
 
-    text <- filter (notElem '.') <$> concatMap words <$> lines <$> readFile filename
-    let hashed = map mkHash text
-    putStr "Total number of words: "
-    print $ length hashed
-    let dHashed = DL.nub hashed
-    putStr "Distinct number of words: "
-    print $ length dHashed
+    g <- getStdGen
+    let n = floor 1e6 -- Max value tested. 1e7 kills GHCI.
+    let c = div n 10
+    let r = take n (randoms g :: [DW.Word32])
+    let bss = DLS.chunksOf c $ map DB.encode r
+    let cards = map (`card` k) bss
+    print cards
 
-    let pq = foldr (`condInsert` k) DPM.empty hashed
-    putStr "Estimated number of disinct words: "
-    print $ estCount k pq
+    let results = map (\x -> fromIntegral c / fromIntegral x) cards
+
+    print results
