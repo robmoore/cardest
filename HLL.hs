2c2d5150a2ff7b30160e259ae096eb755bde51c5
robmoore <rob.moore@gmail.com>
2014-12-16 14:00:16 -0500

    - Fixed bug where count occurred at wrong end of the register value bits. - Fixed bug with numbering in alpha. - Minor renaming and refactoring. - Using random values rather than text values for testing.

--- HLL.hs
+++ HLL.hs
@@ -10,12 +10,11 @@
 
 module HLL where
 
-import           Control.Applicative
 import qualified Control.Monad               as CM (forM_)
 import qualified Control.Monad.ST            as CMS (runST)
-import qualified Data.Bits                   as DB ((.&.), (.|.))
+import           Data.Bits
 -- from bits-extras
-import qualified Data.Bits.Extras            as BE (leadingZeros)
+import qualified Data.Bits.Extras            as BE (trailingZeros)
 import qualified Data.ByteString.Lazy        as BS
 import qualified Data.ByteString.Lazy.Char8  as BC (pack)
 import qualified Data.Digest.XXHash          as XXH (xxHash)
@@ -24,65 +23,72 @@ import qualified Data.Vector.Unboxed         as DVU (Vector, filter, freeze,
 import qualified Data.Vector.Unboxed.Mutable as DVUM (read, replicate, write)
 import qualified Data.Word                   as DW (Word32)
 
-calcN :: Int -> Int
-calcN b = 2 ^ b
+import qualified Data.Binary                 as DB (encode)
+import qualified Data.List.Split             as DLS (chunksOf)
+import           System.Random
+
+calcM :: Int -> Int
+calcM b = 2 ^ b
 
 -- Phase 1: Aggregation
 aggregate :: [BS.ByteString] -> Int -> DVU.Vector DW.Word32
 aggregate vs b = CMS.runST $ do
-  let n = calcN b
-  let mask = fromIntegral n - 1
-  reg <- DVUM.replicate n 0
+  reg <- DVUM.replicate (calcM b) 0
   CM.forM_ vs $ \v -> do
-     let (j, rho) = mkPair v mask
+     let (j, rho) = mkPair v b
      jv <- DVUM.read reg j
      DVUM.write reg j $ max jv rho
   DVU.freeze reg
 
 mkPair :: BS.ByteString -> Int -> (Int, DW.Word32)
-mkPair v m = (j, rho)
-    where h = fromIntegral $ XXH.xxHash v
-          j = fromIntegral $ h DB..&. m -- isolate first b bits for use as index
-          w = h DB..|. m -- turn on first b bits so they don't impact leadingZeros count
-          rho = 1 + BE.leadingZeros w
+mkPair v b = (j, rho)
+    where mask = pred $ calcM b
+          h = fromIntegral $ XXH.xxHash v
+          j = fromIntegral $ h .&. mask -- isolate first b bits for use as index
+          w = h `shiftR` b  -- remove first b bits
+          rho = 1 + BE.trailingZeros w -- count leading zeros from lsb (yes, this is confusing)
 
 -- Phase 2: Result computation
 calcE :: DVU.Vector DW.Word32 -> Int -> Float
-calcE rs n | rawE <= 5 / 2 * ni = if v == 0 then rawE else linearCounting -- {small range correction}
-           | rawE <= (1 / 30) * 2 ^ 32 = rawE -- {intermediate range -- no correction}
+calcE rs m | rawE <= 5 / 2 * mi = if v == 0 then rawE else linearCounting -- {small range correction}
+           | rawE <= 1 / 30 * 2 ^ 32 = rawE -- {intermediate range -- no correction}
            | otherwise = -2 ^ 32 * log (1 - rawE / 2 ^ 32) -- {large range correction}
-           where ni = fromIntegral n
+           where mi = fromIntegral m
                  v = DVU.length $ DVU.filter (== 0) rs -- Let V be the number of registers equal to 0.
                  z =  1 / DVU.sum (DVU.map (\r -> 2 ^^ (-(fromIntegral r))) rs)
-                 rawE = alpha n * ni ^ 2 * z
-                 linearCounting = ni * log (ni / fromIntegral v)
+                 rawE = alpha m * mi ^ 2 * z
+                 linearCounting = mi * log (mi / fromIntegral v)
 
 -- quickCheck (\n -> alpha n >= 0 && alpha n <= 1)
 alpha :: Int -> Float
-alpha n | d == 1 = 0.673 -- n >= 16
-        | d == 2 = 0.697 -- n >= 32
-        | d >= 3 && d <= 8 = 0.709 -- n >= 64
-        | d >= 8 = 0.7213 / (1 + 1.079 / fromIntegral n) -- n >= 128 (in paper)
+alpha m | d == 1 = 0.673 -- m >= 16
+        | d == 2 = 0.697 -- m >= 32
+        | d >= 4 && d < 8 = 0.709 -- m >= 64
+        | d >= 8 = 0.7213 / succ (1.079 / fromIntegral m) -- m >= 128 (in paper)
         | otherwise = 1
-        where d = div n 16
+        where d = div m 16
 
 -- b from the set [4..16]
 card :: [BS.ByteString] -> Int -> Int
-card vs b = round $ calcE e $ calcN b -- aka m in the literature
+card vs b = round $ calcE e m
     where e = aggregate vs b
+          m = calcM b
 
 card' :: [String] -> Int -> Int
 card' vs = card (map BC.pack vs)
 
 main :: IO ()
 main = do
-    let filename = "google-10000-english.txt"
-    text <- map BC.pack <$> filter (notElem '.') <$> concatMap words <$> lines <$> readFile filename
+    let b = 16
 
-    let b = 16 -- TODO: Make this configurable
-    let e = card text b
+    g <- getStdGen
+    let n = floor 1e6 -- Max value tested. 1e7 kills GHCI.
+    let c = div n 10
+    let r = take n (randoms g :: [DW.Word32])
+    let bss = DLS.chunksOf c $ map DB.encode r
+    let cards = map (`card` b) bss
+    print cards
 
-    putStr "Total number of words: "
-    print $ length text
-    putStr "Distinct number of words: "
-    print e -- return cardinality estimate E with typical relative error ±1.04/ m.
+    let results = map (\x -> fromIntegral c / fromIntegral x) cards
+
+    print results
